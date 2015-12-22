b6a8300044cf292c2b39665b30b9ae712d7f9ea3
MalovaAA <nastyamalova1@yandex.ru>
2015-12-22 03:22:12 +0300

    нет тестов,мейна

--- include/monom.h
+++ include/monom.h
@@ -1,1 +1,46 @@
-// класс или структура Монома (double коэффициент, целая свернутая степень)
+#include <iostream>
+
+using namespace std;
+
+class Monom {
+public:
+
+	double coef;
+	int power;
+
+	Monom(double coef = 0, int power = -1){
+	  this->coef = coef;
+	  this->power = power;
+	 }
+
+	Monom(const Monom& m){
+	  this->coef = m.coef;
+	  this->power = m.power;
+	 }
+
+	bool operator==(const Monom &m)
+	{
+	  if (power == m.power) 
+		return true;
+
+	  return false;
+    }
+
+	bool operator<(const Monom &m)
+    {
+	  if (power < m.power) 
+		return true;
+
+	  return false;
+    }
+
+	bool operator>(const Monom &m)
+    {
+	  if (power > m.power) 
+		return true;
+
+	  return false;
+    }
+	
+
+};
